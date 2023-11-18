8cbe4e938c33648198fd484ebd9c8bdf075ee476
Frank Villasenor <frank@theOpenSourceU.org>
2023-11-17 20:31:28 -0600

    Getting things setup. Need to ensure the controls are workign.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import logo from './logo.svg';
 import './App.css';
+import PoemDisplay from "./PoemDisplay";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <PoemDisplay />*/}
+      {/*</header>*/}
+      wlakjkldsjflsdlkfjlsdf
+      <div style={{ "width":'100%', "height":'100%' }}>
+        <PoemDisplay />
+      </div>
     </div>
   );
 }
