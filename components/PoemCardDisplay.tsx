a681cacf542a83ad346fa4b68b2ddacda358041b
Frank Villasenor <frank@theOpenSourceU.org>
2023-12-04 17:34:12 -0600

    New layout direction.

--- components/PoemCardDisplay.tsx
+++ components/PoemCardDisplay.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Card, Stack } from "react-bootstrap";
+import { Card, Stack, Col, Row, Container } from "react-bootstrap";
 import PoemResponse from "../types/PoemResponse";
 import SafeMarkdownToHtml from "../lib/SafeMarkdownToHtml";
 
@@ -9,39 +9,92 @@ export interface PoemDisplaySimpleProps {
 }
 
 const PoemCardDisplay = ({ entries }: PoemDisplaySimpleProps) => {
-  const [poemDataMd, setPoemDataMd] = useState<PoemResponse[]>([]);
+  const [poemDataMd, setPoemDataMd] = useState<PoemResponse[][]>([]);
   useEffect(() => {
     const _poemDataMd: PoemResponse[] = entries.map((itm: PoemResponse) => ({
       poem: SafeMarkdownToHtml(itm.poem),
       title: itm.title,
     }));
-    setPoemDataMd(_poemDataMd);
+
+    const _poemDataMdGrouped = _poemDataMd.reduce((acc, curr, i) => {
+      const chunkIndex = Math.floor(i / 3);
+
+      if (!acc[chunkIndex]) {
+        acc[chunkIndex] = [];
+      }
+
+      acc[chunkIndex].push(curr);
+      return acc;
+    }, [] as PoemResponse[][]);
+
+    setPoemDataMd(_poemDataMdGrouped);
   }, [entries]);
 
   return (
-    <div className={"d-flex flex-wrap"}>
-      <Stack>
-        {poemDataMd.map((entry, idx) => (
-          <Card
-            bg={"light"}
-            text={"dark"}
-            border={"dark"}
+    <Container className={"d-flexbox"}>
+      {poemDataMd.map((entry, idx) => {
+        return (
+          <Row
             key={`PoemCardDisplay-${idx}`}
-            className={"mt-3 shadow-sm"}
+            className={"d-inline-flex w-100 m-2"}
+            style={{ height: "400px" }}
           >
-            <Card.Body>
-              <Card.Title>{entry?.title || "loading"}</Card.Title>
-              <div className="p-2 border rounded">
-                <Card.Text
-                  dangerouslySetInnerHTML={{ __html: entry?.poem || "loading" }}
-                />
-              </div>
-            </Card.Body>
-          </Card>
-        ))}
-      </Stack>
-    </div>
+            {entry.map((itm, idx) => {
+              return (
+                <Col key={`PoemCardDisplay-${idx}`} className={"h-100 w-50"}>
+                  {
+                    <Card
+                      bg={"light"}
+                      text={"dark"}
+                      border={"dark"}
+                      key={`PoemCardDisplay-${idx}`}
+                      className={"mt-3 p-1 shadow-sm overflow-hidden h-100"}
+                    >
+                      <Card.Body>
+                        <Card.Title>{itm?.title || "loading"}</Card.Title>
+                        <div className="p-2 border rounded">
+                          <Card.Text
+                            dangerouslySetInnerHTML={{
+                              __html: itm?.poem || "loading",
+                            }}
+                          />
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  }
+                </Col>
+              );
+            })}
+          </Row>
+        );
+      })}
+    </Container>
   );
+
+  // return (
+  //   <div className={"d-flex flex-wrap g-2"}>
+  //     <Stack>
+  //       {poemDataMd.map((entry, idx) => (
+  //         <Card
+  //           bg={"light"}
+  //           text={"dark"}
+  //           border={"dark"}
+  //           key={`PoemCardDisplay-${idx}`}
+  //           className={"mt-3 p-1 shadow-sm"}
+  //         >
+  //           <Card.Body>
+  //             <Card.Title>{entry?.title || "loading"}</Card.Title>
+  //             <div className="p-2 border rounded">
+  //               <Card.Text
+  //                 dangerouslySetInnerHTML={{ __html: entry?.poem || "loading" }}
+  //               />
+  //             </div>
+  //           </Card.Body>
+  //         </Card>
+  //       ))}
+  //     </Stack>
+  //   </div>
+  // );
 };
 
 export default PoemCardDisplay;
