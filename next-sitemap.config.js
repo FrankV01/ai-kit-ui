d36a5c6e7079d48bcb354ff5dccdb2e16fdb8d93
Frank Villasenor <frank@theOpenSourceU.org>
2023-12-13 13:54:04 -0600

    sitemap generation.

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.BASE_URL || "https://poems.theOpenSourceU.org",
+  generateRobotsTxt: true,
+
+  // ...other options
+};
