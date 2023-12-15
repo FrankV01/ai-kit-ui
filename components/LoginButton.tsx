4bf31592fa911af9f1c8660ef13d3b086904aa79
Frank Villasenor <frank@theOpenSourceU.org>
2023-12-15 10:34:24 -0600

    note

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,13 +1,15 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "react-bootstrap";
 
+// Under_dev;
+//  it seemed to send the user to my api rather than the built in one.
 export default function Component() {
   const { data: session } = useSession();
 
   if (session && session.user) {
     return (
       <div className={"float-end"}>
-        <Button className={" text-dark"} onClick={() => signOut()}>
+        <Button className={"text-dark"} onClick={() => signOut()}>
           Signed in as {session.user.email}
         </Button>
       </div>
@@ -15,11 +17,8 @@ export default function Component() {
   }
   return (
     <div title={"Not signed in"} className={"float-end"}>
-      <Button
-        className={"button small text-dark outline button-outline"}
-        onClick={() => signIn()}
-      >
-        Sign In
+      <Button className={"button text-dark"} onClick={() => signIn()}>
+        Sign In/Up
       </Button>
     </div>
   );
