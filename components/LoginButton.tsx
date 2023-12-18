b4883baf300a6b41fe331d1b27ab152447ad37f2
Frank Villasenor <frank@theOpenSourceU.org>
2023-12-18 10:25:43 -0600

    Adjustment.

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -6,14 +6,12 @@ import { useEffect } from "react";
 export default function LoginButton() {
   const { data: session } = useSession();
   useEffect(() => {
-    console.log("use effect");
-    // Under_dev: We need to move this. I think.
-    //  It should be when the user is created. Not (1) every time
-    //  and (2) when we're actually creating the user.
-    RecordLogin().then(() => {
-      console.log("recordData:then");
-    });
-  }, []);
+    if (session && session.user) {
+      RecordLogin().then(() => {
+        console.log("recordData:then");
+      });
+    }
+  }, [session]);
 
   if (session && session.user) {
     return (
