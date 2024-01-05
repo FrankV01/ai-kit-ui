b349713cccc4995d394525ce8bce6b8e51156148
Frank Villasenor <frank@theOpenSourceU.org>
2024-01-05 09:16:55 -0600

    Need to redeploy to ensure everything is equal.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import React, { Suspense } from "react";
 import Loading from "./loading";
 import PoemRow from "../components/poems/PoemRow";
 import { getPoemIdList } from "../lib/ApiActions";
+import PoemCardDisplay from "../components/PoemCardDisplay";
+import PoemResponse from "../types/PoemResponse";
+
+const url = process.env.API_URL ? `${process.env.API_URL}/poems` : ""; //"http://localhost:3001/poems";
 
 // Ok, so why does / go to page.tsx and not index.tsx?
 //  It's because it's at the root of app and page.tsx is "index.tsx" in next.js
@@ -17,40 +21,74 @@ import { getPoemIdList } from "../lib/ApiActions";
  * This is an indicator for Next.js
  */
 export const dynamic = "force-dynamic";
+//
+// export default async function Home() {
+//   //const poemData: number[] = await getPoemIdList();
+//   console.log("loading home...");
+//   let poemData: number[] = [];
+//   try {
+//     console.log("getting data...");
+//     poemData = await getPoemIdList();
+//   } catch (err) {
+//     console.log("error while getting data...");
+//     let message = "unknown";
+//     if (err instanceof Error) message = err.message;
+//     else message = String(err);
+//     console.warn("Home:poemData", message);
+//   }
+//
+//   console.log("grouping data...");
+//   const _poemDataGrouped = poemData.reduce((acc, curr, i) => {
+//     const chunkIndex = Math.floor(i / 3.0);
+//
+//     if (!acc[chunkIndex]) {
+//       acc[chunkIndex] = [];
+//     }
+//
+//     acc[chunkIndex].push(curr);
+//     return acc;
+//   }, [] as number[][]);
+//
+//   console.log("Generating rows...");
+//   const rows = _poemDataGrouped.map((itm) => (
+//     <PoemRow key={`poemRow-${itm[0]}`} poemIds={itm} />
+//   ));
+//
+//   return (
+//     <div className={styles.outline}>
+//       <div className={"container"}>
+//         <Suspense fallback={<Loading />}>{rows}</Suspense>
+//       </div>
+//     </div>
+//   );
+// }
+
+async function getData(): Promise<PoemResponse[]> {
+  if (!url) {
+    throw new Error("Invalid environment configs");
+  }
+  console.log(`Fetching data from ${url}`);
+  const res = await fetch(url, {
+    //cache: "no-cache",
+    cache: "no-store",
+    //next: { revalidate: 3600 / 2 },
+  });
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    console.log(`Failed to fetch data from ${url}`);
+    throw new Error("Failed to fetch data"); //Stupid fucking error boundry.
+  }
+  return await res.json();
+}
 
 export default async function Home() {
-  //const poemData: number[] = await getPoemIdList();
-
-  let poemData: number[] = [];
-  try {
-    poemData = await getPoemIdList();
-  } catch (err) {
-    let message = "unknown";
-    if (err instanceof Error) message = err.message;
-    else message = String(err);
-    console.warn("Home:poemData", message);
-  }
-
-  const _poemDataGrouped = poemData.reduce((acc, curr, i) => {
-    const chunkIndex = Math.floor(i / 3.0);
-
-    if (!acc[chunkIndex]) {
-      acc[chunkIndex] = [];
-    }
-
-    acc[chunkIndex].push(curr);
-    return acc;
-  }, [] as number[][]);
-
-  const rows = _poemDataGrouped.map((itm) => (
-    <PoemRow key={`poemRow-${itm[0]}`} poemIds={itm} />
-  ));
-
-  return (
-    <div className={styles.outline}>
-      <div className={"container"}>
-        <Suspense fallback={<Loading />}>{rows}</Suspense>
+  const oldVersion = true;
+  if (oldVersion) {
+    const poemData: PoemResponse[] = await getData();
+    return (
+      <div className={styles.outline}>
+        <PoemCardDisplay entries={poemData} />
       </div>
-    </div>
-  );
+    );
+  }
 }
--- lib/ApiActions.ts
+++ lib/ApiActions.ts
@@ -9,23 +9,27 @@ export async function demandPoem(): Promise<string> {
   return Promise.resolve("Demand example; ".repeat(20));
 }
 
+const log = console.log.bind(console);
+
 export async function getPoemIdList(
   pageNum: number = 1,
   pageSize: number = 1000,
 ): Promise<number[]> {
+  log("getPoemIdList:paging", pageNum, pageSize);
   const base = (await EvtMgr()).BASE_URL;
+  log("getPoemIdList:base", base);
   //const url = `${base}/poems?pageNum=${pageNum}&pageSize=${pageSize}`;
   const url = `${base}/poems/ids?pageNum=${pageNum}&pageSize=${pageSize}`;
   if (!url) {
     throw new Error("Invalid environment configs");
   }
-  console.log(`Fetching data from ${url}`);
+  log(`Fetching data from ${url}`);
   const res = await fetch(url, {
     cache: "no-store",
   });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    console.log(`Failed to fetch data from ${url}`);
+    log(`Failed to fetch data from ${url}`);
     throw new Error("Failed to fetch data"); //Stupid fucking error boundry.
   }
   return await res.json();
@@ -36,6 +40,7 @@ export async function getPoemById(
   pageNum: number = 1,
   pageSize: number = 100000,
 ): Promise<PoemResponse> {
+  log("getPoemIdList:getPoemById", id, pageNum, pageSize);
   const base = (await EvtMgr()).BASE_URL;
   const url = `${base}/poem/${id}?pageNum=${pageNum}&pageSize=${pageSize}`;
   if (!url) {
@@ -54,6 +59,7 @@ export async function getPoemById(
 }
 
 export async function queueRequest(formData: FormData) {
+  log("getPoemIdList:queueRequest", formData);
   const session = await getServerSession();
   if (!session || !session.user) {
     console.warn("queueRequest::No session found.");
@@ -87,6 +93,7 @@ export async function queueRequest(formData: FormData) {
 }
 
 export async function RecordLogin(user: User | AdapterUser) {
+  log("getPoemIdList:RecordLogin");
   const RecordLoginMsg = "RecordLogin::PUT";
 
   const baseUrl = (await EvtMgr()).BASE_URL;
