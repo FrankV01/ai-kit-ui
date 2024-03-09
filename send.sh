f5a13ffd3dbd7a17b890403fdf2f78b169996ea9
Frank Villasenor <frank@theOpenSourceU.org>
2024-03-08 19:52:18 -0600

    Send file updated.

--- send.sh
+++ send.sh
@@ -1,18 +1,10 @@
-#docker build . -t ai-platform-ui:dev --target development #Add the environment vars here.
-#docker build . -t ai-platform-ui:latest -t ai-platform-ui:production --target production
-#
-#docker tag ai-platform-ui:dev registry.digitalocean.com/tosu/ai-platform-ui:dev
-#docker tag ai-platform-ui:latest registry.digitalocean.com/tosu/ai-platform-ui:latest
-#docker tag ai-platform-ui:latest registry.digitalocean.com/tosu/ai-platform-ui:production
-#docker push --all-tags registry.digitalocean.com/tosu/ai-platform-ui
+docker build . -t ai-kit-ui:dev --target development #Add the environment vars here.
+docker build . -t ai-kit-ui:latest -t ai-kit-ui:production --target production
 
+docker tag ai-kit-ui:dev registry.digitalocean.com/tosu/ai-kit-ui:dev
+docker tag ai-kit-ui:latest registry.digitalocean.com/tosu/ai-kit-ui:latest
+docker tag ai-kit-ui:latest registry.digitalocean.com/tosu/ai-kit-ui:production
+docker push --all-tags registry.digitalocean.com/tosu/ai-kit-ui
 
-docker build . -t poems-ui:dev --target development #Add the environment vars here.
-docker build . -t poems-ui:latest -t poems-ui:production --target production
-
-docker tag poems-ui:dev registry.digitalocean.com/tosu/poems-ui:dev
-docker tag poems-ui:latest registry.digitalocean.com/tosu/poems-ui:latest
-docker tag poems-ui:latest registry.digitalocean.com/tosu/poems-ui:production
-docker push --all-tags registry.digitalocean.com/tosu/poems-ui
 
 ## doctl registry login
