b10e0d891c4ce218a70a4e3bb64d208a076dd467
Frank Villasenor <frank@theOpenSourceU.org>
2024-04-18 14:26:12 -0500

    Setup Jest for the ai-kit-ui.

---
+++ jest.config.ts
@@ -0,0 +1,37 @@
+module.exports = {
+  // The root directory of your project.
+  rootDir: __dirname,
+
+  // The test environment that will be used.
+  testEnvironment: "node",
+
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  //testRegex: "(/__tests__/.*|(.|/)(test|spec)).tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  // The test files that should be run.
+  testMatch: ["**/__tests__/**/*.ts", "**/__tests__/**/*.tsx"],
+
+  // The reporters that should be used.
+  reporters: ["default"],
+
+  // The coverage reporter that should be used.
+  coverageReporters: ["json", "lcov"],
+
+  // The path to the coverage report file.
+  coveragePathIgnorePatterns: ["/node_modules/", "/__tests__/", "/__mocks__/"],
+
+  // The maximum number of test files that should be run in parallel.
+  //maxWorkers: 1,
+
+  // The number of milliseconds to wait before killing a test that is running too long.
+  testTimeout: 10000,
+
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+};
