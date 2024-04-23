1757cb763ffa3cb0fa16c8ac8547fe8538d09ad4
Frank Villasenor <frank@theOpenSourceU.org>
2024-04-23 10:53:27 -0500

    Revision seemed to work; this commit includes the commented old code.

--- components/__tests__/poems/PoemCard.tsx
+++ components/__tests__/poems/PoemCard.tsx
@@ -56,11 +56,18 @@ describe("PoemCard", () => {
 
   it("renders loading state initially", () => {
     act(() => {
-      const { getByTestId } = render(<PoemCard id={1} />);
-      waitFor(() => {
-        expect(getByTestId("loading-notif")).toBeInTheDocument();
-      });
+      render(<PoemCard id={1} />);
     });
+    waitFor(() => {
+      expect(screen.getByTestId("loading-notif")).toBeInTheDocument();
+    });
+
+    // act(() => {
+    //   const { getByTestId } = render(<PoemCard id={1} />);
+    //   waitFor(() => {
+    //     expect(getByTestId("loading-notif")).toBeInTheDocument();
+    //   });
+    // });
   });
 
   it("renders poem data when loaded", async () => {
