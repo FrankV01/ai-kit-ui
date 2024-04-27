d66dda9a6ea7e09cd8aa55ca64d0bfa9d84daa12
Frank Villasenor <frank@theOpenSourceU.org>
2024-04-27 15:09:42 -0500

    Notes and doc.

--- components/server/AddNonce.tsx
+++ components/server/AddNonce.tsx
@@ -6,6 +6,13 @@ type AddNonceProps = {
   children: React.ReactNode;
 };
 
+/**
+ * The intention is to add the CSP nonce to the children of this component.
+ * but it doesn't seem to work as expected. Perhaps because of the
+ * use server directive. Not sure.
+ * @param children
+ * @constructor
+ */
 export default async function AddNonce({ children }: AddNonceProps) {
   const nonce: string = (headers().get("x-nonce") as string) || "[missing]";
   return (
