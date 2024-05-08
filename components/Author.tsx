0a4dc9944254b3d09c9bdd9ef40059afd3ba3585
Frank Villasenor <frank@theOpenSourceU.org>
2024-05-08 13:22:31 -0500

    Make the author component a bit more flexible.

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,10 +1,20 @@
-export default function Author({ className }: { className?: string }) {
-  return (
-    <a
-      href={"mailto:frank.villasenor@gmail.com;frank@theOpenSourceU.org"}
-      className={className || ""}
-    >
-      Frank Villasenor
-    </a>
-  );
+export type AuthorProps = {
+  includeEmailLink: boolean;
+  className?: string;
+};
+
+const name = "Frank Villasenor";
+export default function Author({ includeEmailLink, className }: AuthorProps) {
+  if (includeEmailLink) {
+    return (
+      <a
+        href={"mailto:frank.villasenor@gmail.com;frank@theOpenSourceU.org"}
+        className={className || ""}
+      >
+        {name}
+      </a>
+    );
+  } else {
+    return <span className={className || ""}>{name}</span>;
+  }
 }
