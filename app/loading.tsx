fa0d20c48512ff254e1f437c53fdda1f5fdc242c
Frank Villasenor <frank@theOpenSourceU.org>
2024-05-11 10:12:18 -0500

    Alternative loading via prop.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { PoemLoadingColumn } from "../components/poems/PoemLoadingColumn";
 
-export default function Loading() {
+export default function Loading({ poemsMode }: { poemsMode: boolean }) {
+  if (!poemsMode)
+    return (
+      <div>
+        <div className="spinner-border m-5" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+
   return <PoemLoadingColumn includeLayout={true} />;
 }
