d7f2d0683fad05f7b8c671e3f78b3ba085381cff
Frank Villasenor <frank@theOpenSourceU.org>
2024-05-10 21:43:28 -0500

    Better menu.

--- components/whitelabel/ModernMode.tsx
+++ components/whitelabel/ModernMode.tsx
@@ -19,7 +19,7 @@ async function ModernHeader() {
   return (
     <div className={"row"}>
       <header className={"col"}>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary flex-fill">
+        <nav className="navbar navbar-expand-lg bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
               Navbar
@@ -28,77 +28,26 @@ async function ModernHeader() {
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/chat">
-                    Chat
-                  </a>
-                </li>
-                <li className="nav-item invisible dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item  invisible">
-                  <a className="nav-link disabled" aria-disabled="true">
-                    Disabled
-                  </a>
-                </li>
-              </ul>
-              <form className="d-flex  invisible" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+                <a className="nav-link disabled">Disabled</a>
+              </div>
             </div>
           </div>
         </nav>
