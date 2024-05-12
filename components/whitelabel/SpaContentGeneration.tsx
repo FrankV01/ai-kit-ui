9705d2787f0671f82cf6c1a180c5ccd0f7484bf1
Frank Villasenor <frank@theOpenSourceU.org>
2024-05-12 18:23:55 -0500

    Concept for the session / generated content.

--- components/whitelabel/SpaContentGeneration.tsx
+++ components/whitelabel/SpaContentGeneration.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 /**
  * This should generate and manage the content component
@@ -7,5 +7,24 @@ import React from "react";
  * @constructor
  */
 export async function SpaContentGeneration() {
-  return <div className={"container"}>Content Generation</div>;
+  return (
+    <div className={"container m-2"}>
+      <div className={"row"}>
+        <div className={"p-1 col-9 text-center"}>
+          <h3>title here</h3>
+        </div>
+        <div className={"p-1 col-3 text-end"}>Date Published</div>
+      </div>
+      <div className="row">
+        <div className="col ">
+          <div className={""}>
+            Create boxes that hold the content. Some comment elements, maybe
+            eventually configuration, are date created, title, tags, and the
+            content itself. The content is markdown and needs to be rendered as
+            such.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
