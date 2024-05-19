6897273f542d611a19aa544ea6553cc3071914ec
Frank Villasenor <frank@theOpenSourceU.org>
2024-05-19 15:20:03 -0500

    Fix the assertion bugs from the react-boostrap elimination.

--- components/poems/PoemSubmissionNotice.tsx
+++ components/poems/PoemSubmissionNotice.tsx
@@ -1,7 +1,7 @@
 export default function PoemSubmissionNotice() {
   return (
     <div>
-      <div className={"alert alert-primary"}>
+      <div role={"alert"} className={"alert alert-primary"}>
         <p className={"m-0 p-0"}>
           <strong>Note:</strong> For the time being submitted entries are queued
           for review. The review is done manually. We appreciate your patience.
