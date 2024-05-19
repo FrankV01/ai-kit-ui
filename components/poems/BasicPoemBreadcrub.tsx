7a189c414102c285c6e03faf4ccf34be37d39b88
Frank Villasenor <frank@theOpenSourceU.org>
2024-05-19 15:18:54 -0500

    Fix the assertion bugs from the react-boostrap elimination.

--- components/poems/BasicPoemBreadcrub.tsx
+++ components/poems/BasicPoemBreadcrub.tsx
@@ -8,15 +8,23 @@ export type BasicPoemBreadcrubProps = {
 export default function BasicPoemBreadcrub(props: BasicPoemBreadcrubProps) {
   const poemTitle = props.poemTitle || "Viewing Poem";
   return (
-    <nav aria-label="breadcrumb" className={"text-center fw-bold"}>
+    <nav
+      aria-label="breadcrumb"
+      role={"breadcrumb"}
+      className={"text-center fw-bold"}
+    >
       <ol className="breadcrumb">
         <li className="breadcrumb-item">
           <a title={"Go to Home Page"} href="/">
             <Icons.ArrowLeftCircleFill /> Home
           </a>
         </li>
-        <li className="breadcrumb-item active" aria-current="page">
-          <a href="#" title={"Poem Title"}>
+        <li
+          className="breadcrumb-item active"
+          role={"breadcrumb-item"}
+          aria-current="page"
+        >
+          <a href="#" role={"link"} title={"Poem Title"}>
             <Icons.EnvelopeOpenHeart />: {poemTitle}
           </a>
         </li>
--- components/poems/LoadingPoemBody.tsx
+++ components/poems/LoadingPoemBody.tsx
@@ -2,6 +2,26 @@
 import BasicPoemBreadcrub from "./BasicPoemBreadcrub";
 import React from "react";
 
+type RandomDynamicPlaceHolderType = {
+  size: "xs" | "sm" | "md";
+};
+type DynamicPlaceHolderType = RandomDynamicPlaceHolderType & {
+  n: number;
+};
+
+const DynamicPlaceHolder = ({ n, size }: DynamicPlaceHolderType) => (
+  <>
+    <span
+      data-role="status-placeholder"
+      role={"status"}
+      className={`placeholder placeholder-${size}-${n}`}
+    />{" "}
+  </>
+);
+const RandomDynamicPlaceHolder = ({ size }: RandomDynamicPlaceHolderType) => (
+  <DynamicPlaceHolder size={size} n={Math.floor(Math.random() * 6) + 1} />
+);
+
 const LoadingPoemBody = () => {
   return (
     <div className="container" role={"main"}>
@@ -10,65 +30,65 @@ const LoadingPoemBody = () => {
         <div className="col-lg-8 col-md-10 col-sm-12">
           <h2>
             <span className="placeholder-wave">
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-4" />
-            </span>{" "}
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={2} />{" "}
+              <DynamicPlaceHolder size={"xs"} n={4} />
+            </span>
           </h2>
           <BasicPoemBreadcrub poemTitle={"Loading"} />
           <div className={"body p-2 my-3"}>
             <span className="placeholder-glow">
-              <span className="placeholder-xs-6" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-5" />{" "}
-              <span className="placeholder-xs-6" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-5" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-3" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-6" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-5" />{" "}
-              <span className="placeholder-xs-6" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-5" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-3" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-6" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-5" />{" "}
-              <span className="placeholder-xs-6" />{" "}
-              <span className="placeholder-xs-2" />{" "}
-              <span className="placeholder-xs-5" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-3" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-4" />{" "}
-              <span className="placeholder-xs-4" />{" "}
+              <DynamicPlaceHolder size={"xs"} n={6} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={2} />
+              <DynamicPlaceHolder size={"xs"} n={5} />
+              <DynamicPlaceHolder size={"xs"} n={6} />
+              <DynamicPlaceHolder size={"xs"} n={2} />
+              <DynamicPlaceHolder size={"xs"} n={5} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={3} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={6} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={2} />
+              <DynamicPlaceHolder size={"xs"} n={5} />
+              <DynamicPlaceHolder size={"xs"} n={6} />
+              <DynamicPlaceHolder size={"xs"} n={2} />
+              <DynamicPlaceHolder size={"xs"} n={5} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={3} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={6} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={2} />
+              <DynamicPlaceHolder size={"xs"} n={5} />
+              <DynamicPlaceHolder size={"xs"} n={6} />
+              <DynamicPlaceHolder size={"xs"} n={2} />
+              <DynamicPlaceHolder size={"xs"} n={5} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={3} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
+              <DynamicPlaceHolder size={"xs"} n={4} />
             </span>
           </div>
           <h4 className={"text-body-secondary"} title={"loading widget"}>
             <span className="placeholder-glow">
-              <span className="placeholder-md-12" />
+              <DynamicPlaceHolder size={"md"} n={12} />
             </span>
           </h4>
           <div title={"loading widget"} className={"small text-body-secondary"}>
             <span className="placeholder-glow">
-              <span className="placeholder-md-12" />
+              <DynamicPlaceHolder size={"md"} n={12} />
             </span>
           </div>
           <div className={"mt-3 small text-secondary float-end"}>
             <span className="placeholder-glow">
-              <span className="placeholder-md-12" />
+              <DynamicPlaceHolder size={"md"} n={12} />
             </span>
           </div>
         </div>
