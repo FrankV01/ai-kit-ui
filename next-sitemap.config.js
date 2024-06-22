a4236784c5419ba9a5cf71d8b3d09c9024f32f35
Frank Villasenor <frank@theOpenSourceU.org>
2024-06-22 17:10:26 -0500

    plan and thoughts.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.BASE_URL || "https://poems.theOpenSourceU.org",
+  // TODO: Make this more dynamic. For now, it's fine.
+  // Under_Dev: Need to put the serve-url in storage some place.
+  //  Is this use during run time or build time?
+  siteUrl: "https://poems.theOpenSourceU.org", // Ok, so where can we store this? Where else might we need this?
   generateRobotsTxt: true,
   generateIndexSitemap: true,
 
